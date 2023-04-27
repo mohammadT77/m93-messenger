09386325da3ce3fd5866b8410ea16417925ac26d
matehrani <t.mohammadamin@gmail.com>
2023-04-27 10:32:38 +0200

    impl core.views, core.base.py, change menu_route

---
+++ core/views.py
@@ -0,0 +1,29 @@
+
+def login():
+    username = input("Username: ")
+    password = input("Password: ")
+
+    # TODO
+
+def register():
+    username = input("Username: ")
+    firstname = input("Firstname: ")
+    lasstname = input("Lastname: ")
+    password = input("Password: ")
+
+    # TODO
+
+def logout():
+    pass
+
+def create_new_message():
+    pass
+
+def inbox():
+    pass
+
+def sent():
+    pass
+
+def draft():
+    pass
