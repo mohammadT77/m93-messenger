fd2601bea197e21670c67a17e288f8ff20c470e2
matehrani <t.mohammadamin@gmail.com>
2023-04-28 10:06:45 +0200

    fix create_table, rename dict->to_dict

--- data_manager/db_manager.py
+++ data_manager/db_manager.py
@@ -9,29 +9,43 @@ class DBManager(BaseManager):
         self._db_config = config['db_config']
         self.__conn = pg.connect(**self._db_config)
 
-    @staticmethod
-    def _get_create_table_sql(model_cls: type):
-        assert getattr(model_cls, 'COLUMNS', None), "Error"
-        assert getattr(model_cls, 'TABLE_NAME', None), "Error"
-        assert issubclass(model_cls, BaseModel)
-
-        sql = f"CREATE TABLE {model_cls.TABLE_NAME} ("
-        for col in model_cls._get_columns().values():
-            sql += " ".join(col) + ','
-        sql += ')'
-
-        return sql
     
     def create_table(self, model_cls: type):
+        assert issubclass(model_cls, BaseModel)
+
         with self.__conn.cursor() as curs:
-            sql = self._get_create_table_sql(model_cls)
-            curs.execute(sql)
+            cols_dict = model_cls._get_columns()
+            sql_cols = ','.join([" ".join(v) for v in cols_dict.values()])
+            curs.execute(f"CREATE TABLE {model_cls.TABLE_NAME} ({sql_cols});", )
         
         self.__conn.commit()
     
+    def _check_table_exists(self,  model_cls: type):
+        with self.__conn.cursor() as curs:
+            curs.execute("SELECT * FROM information_schema.tables WHERE table_name=%s",
+                        (model_cls.TABLE_NAME,))
+            return bool(curs.fetchone())
+
 
     def create(self, m: BaseModel):
-        pass
+        if not self._check_table_exists(m.__class__):
+            self.create_table(m.__class__)
+        
+        model_data = m.to_dict()  # {'_id':1, 'username':'akbar', ...}
+        converter = lambda x: f"'{x}'" if isinstance(x, str) else str(x)
+
+        with self.__conn.cursor() as curs:
+            keys = ','.join(model_data.keys())
+            values = ','.join(map(converter, model_data.values())) # 1, 'akbar', 'akbar1',... -> 1, 'akbar', 'akbar1' -> "1, 'akbar', 'akbar1'"
+            curs.execute(f"INSERT INTO {m.TABLE_NAME} ({keys}) VALUES ({values}) RETURNING _id")
+            new_model_id = curs.fetchone()
+            m._id = new_model_id
+        
+        self.__conn.commit()
+        return new_model_id
+
+       
+        
 
     def read(self, id: int, model_cls: type) -> BaseModel:
         assert issubclass(model_cls, BaseModel)
@@ -48,7 +62,10 @@ class DBManager(BaseManager):
 
 
     def update(self, m: BaseModel) -> None:
-        pass
+        assert getattr(m, '_id', None)
+        # m.to_dict()
+        # m.TABLE_NAME
+        "UPDATE tablename x WHERE x, SET y"
 
 
     def delete(self, id: int, model_cls: type) -> None:
