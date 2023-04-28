c3f18004a7b7cc087d1a2b701c8ef3e1d2c2de77
matehrani <t.mohammadamin@gmail.com>
2023-04-28 15:48:55 +0200

    reformat

--- main.py
+++ main.py
@@ -3,14 +3,3 @@ from menu_route import MENU_ROUTE
 
 menu_root = generate_menu(MENU_ROUTE)
 menu_root()
-# db_config = {
-#     'dbname': 'postgres',
-#     'host':'localhost',
-#     'user':'postgres',
-#     'password': 'postgres'
-# }
-
-# from data_manager.db_manager import DBManager
-# from core.models import User, Message
-# messages = Message.get_messages_by_status(sender=2)
-# print(*list(messages), sep='\n')
